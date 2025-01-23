ceaaadb8a914d653eb5ab5e97af22ec44d7909bb
Rodri10s <ramonrodrigues838@gmail.com>
2025-01-23 08:15:57 -0300

    Criação das classes, Produto e Venda, além da criação da tabela muitos para muitos VendaProduto

---
+++ src/main/java/basicas/VendaProduto.java
@@ -0,0 +1,57 @@
+package basicas;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_venda_produto")
+public class VendaProduto implements Serializable {
+
+    @EmbeddedId
+    private VendaProdutoPK id = new VendaProdutoPK();
+
+    public VendaProduto() {
+    }
+
+    public VendaProduto(Venda venda, Produto produto) {
+        id.setProduto(produto);
+        id.setVenda(venda);
+    }
+
+    public VendaProdutoPK getId() {
+        return id;
+    }
+
+    public void setId(VendaProdutoPK id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        VendaProduto other = (VendaProduto) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+}
--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -1,12 +1,13 @@
 package principal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import basicas.Categoria;
 import basicas.Cliente;
 import basicas.Endereco;
+import basicas.Produto;
+import basicas.Venda;
+import basicas.VendaProduto;
 import dao.GetEntityManager;
 
 public class Principal {
@@ -14,36 +15,75 @@ public class Principal {
 	public static void main(String[] args) {
 		EntityManager em = GetEntityManager.getJpaConnection();
 
-		Cliente cli = new Cliente();
+		// Cliente cli = new Cliente();
 
-		Categoria cat = new Categoria();
-		cat.setDescricao("Descrição 1");
+		// Categoria cat = new Categoria();
+		// cat.setDescricao("Descrição 1");
+
+		// em.getTransaction().begin();
+		// em.persist(cat);
+		// em.getTransaction().commit();
+
+		// cli.setNome("Ramon");
+		// cli.setEmail("ramonrodrigues838@gmail.com");
+		// cli.setFone("(87) 9 9645-7790");
+		// cli.setCategoria(cat);
+
+		// Endereco end = new Endereco();
+		// end.setEstado("Pernambuco");
+		// end.setCidade("Petrolândia");
+		// end.setCep("56460-000");
+		// end.setBairro("Centro");
+		// end.setRua("Vicente Guimarães");
+		// end.setNumero("84");
+
+		// cli.setEndereco(end);
+
+		// em.getTransaction().begin();
+		// em.persist(end);
+		// em.persist(cli);
+		// em.getTransaction().commit();
+
+		// em.close();
+
+		Produto produto1 = new Produto();
+		produto1.setDescricao("Chave");
+		produto1.setEstoque(10);
+		produto1.setValor(5.0);
+
+		Produto produto2 = new Produto();
+		produto2.setDescricao("Cadeado");
+		produto2.setEstoque(10);
+		produto2.setValor(10.0);
+
+		Produto produto3 = new Produto();
+		produto3.setDescricao("Chaveiro");
+		produto3.setEstoque(15);
+		produto3.setValor(2.0);
+
+		Venda venda1 = new Venda();
+		venda1.setDescricao("concluido");
+
+		Venda venda2 = new Venda();
+		venda2.setDescricao("em andamento");
+
+		VendaProduto vp1 = new VendaProduto(venda1, produto1);
+		VendaProduto vp2 = new VendaProduto(venda1, produto2);
+		VendaProduto vp3 = new VendaProduto(venda2, produto3);
 
 		em.getTransaction().begin();
-		em.persist(cat);
-		em.getTransaction().commit();
-
-		cli.setNome("Ramon");
-		cli.setEmail("ramonrodrigues838@gmail.com");
-		cli.setFone("(87) 9 9645-7790");
-		cli.setCategoria(cat);
-
-		Endereco end = new Endereco();
-		end.setEstado("Pernambuco");
-		end.setCidade("Petrolândia");
-		end.setCep("56460-000");
-		end.setBairro("Centro");
-		end.setRua("Vicente Guimarães");
-		end.setNumero("84");
-
-		cli.setEndereco(end);
-
-		em.getTransaction().begin();
-		em.persist(end);
-		em.persist(cli);
+		em.persist(produto1);
+		em.persist(produto2);
+		em.persist(produto3);
+		em.persist(venda1);
+		em.persist(venda2);
+		em.persist(vp1);
+		em.persist(vp2);
+		em.persist(vp3);
 		em.getTransaction().commit();
 
 		em.close();
+
 	}
 
 }
